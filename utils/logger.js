cfa0914d8d46769964b5f52bb66d9415d30336a5
Fakeitem <non_scoobydoo@hotmail.com>
2021-02-12 23:06:25 +0700

    Fix function name

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,7 @@
 const moment = require('moment')
 const colors = require('colors')
 
-const evnetLog = request => {
+const eventLog = request => {
   const { responded, received, id } = request.info
   const method = request.method.toUpperCase()
   const url = `${request.url}`
@@ -34,5 +34,5 @@ const evnetLog = request => {
 }
 
 module.exports = {
-  evnetLog
+  eventLog
 }
