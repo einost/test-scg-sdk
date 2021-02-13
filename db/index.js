5405da234ec36756fa49507bdce0c83c290c99ed
Fakeitem <non_scoobydoo@hotmail.com>
2021-02-13 17:52:39 +0700

    Export Stock model

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   VendingMachine: require('./model/VendingMachine'),
-  Product: require('./model/Product')
+  Product: require('./model/Product'),
+  Stock: require('./schema/Stock')
 }
